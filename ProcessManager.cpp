9eda38aecd0efd9dd1d682ddb52779288e4aa14e
dashxio <dashxio@qq.com>
2024-10-12 18:58:40 +0800

    Remove unnecessary files

--- ProcessManager.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "ProcessManager.h"
-
-std::atomic<bool> running(true);
-
-std::unordered_map<std::string, std::function<void()>> ProcessManager::func_map = {};
-MTQueue<std::string> ProcessManager::key_list = MTQueue<std::string>();
-std::vector<std::thread> ProcessManager::thread_list = {};
